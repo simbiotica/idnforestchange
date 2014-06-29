31818da77f8c8cfc705c5aa5741ea74a65b3603a
David Gonzalez <david@simbiotica.es>
2014-06-29 13:07:49 +0200

    added forest change layer

--- app/scripts/layers/ForestChangeLayer.js
+++ app/scripts/layers/ForestChangeLayer.js
@@ -13,7 +13,9 @@ define([
       this._super();
       this.dataMaxZoom = 12;
       this.name = 'forestChange';
-      this.urlTemplate = 'http://earthengine.google.org/static/hansen_2013/gfw_loss_year/%z/%x/%y.png';
+      //this.urlTemplate = 'http://earthengine.google.org/static/hansen_2013/gfw_loss_year/%z/%x/%y.png';
+      this.urlTemplate = 'https://s3.amazonaws.com/idn/idnfc2/%z/%x/%y.png';
+
 
       mps.subscribe('filter/change', _.bind(function(params) {
         this.params = params;
@@ -25,21 +27,59 @@ define([
     filterCanvasImage: function(imgdata, w, h, z) {
       var components = 4;
 
+
       for(var i = 0; i < w; ++i) {
         for(var j = 0; j < h; ++j) {
 
           var pixelPos = (j * w + i) * components;
-          var yearLoss = imgdata[pixelPos];
-          var yearStart = 2001;
-          var yearEnd = 2013;
+          var band = imgdata[pixelPos];
 
-          yearLoss = 2000 + yearLoss;
+          if (band>0 && band<16) {
+            imgdata[pixelPos] = 0;
+            imgdata[pixelPos + 1]  = 55;
+            imgdata[pixelPos + 2] = 0;
+            imgdata[pixelPos + 3] = 100;
+          }
 
-          if (imgdata[pixelPos + 1] > 10 && (yearLoss >= yearStart && yearLoss < yearEnd)) {
-            imgdata[pixelPos] = 220;
-            imgdata[pixelPos + 1] = 102;
-            imgdata[pixelPos + 2] = 153;
-            imgdata[pixelPos + 3] = (z < 13) ? (12/z) * 255 : 255;
+          if (band>0 && band<4) {
+            //console.log(band);
+            imgdata[pixelPos] = 0;
+            //imgdata[pixelPos + 1] = band*10+200;
+            switch (band){
+              case 1:
+                imgdata[pixelPos + 1]  = 155;
+                break;
+              case 2:
+                imgdata[pixelPos + 1]  = 255;
+                break;
+              case 3:
+                imgdata[pixelPos + 1]  = 55;
+                break;
+            }
+            imgdata[pixelPos + 2] = 0;
+            imgdata[pixelPos + 3] = 100;
+          } else if (band%3===1 && band<13) {
+            switch (band){
+              case 4:
+                imgdata[pixelPos] = 255;
+                break;
+              case 7:
+                imgdata[pixelPos] = 155;
+                break;
+              case 10:
+                imgdata[pixelPos] = 255;
+                break;
+            }
+            //imgdata[pixelPos] = band*255;
+            imgdata[pixelPos + 1] = 0;
+            imgdata[pixelPos + 2] = 0;
+            imgdata[pixelPos + 3] = 205;
+          } else if (band%3===2) {
+            //console.log(band);
+            imgdata[pixelPos] = band*10+100;
+            imgdata[pixelPos + 1] = 255;
+            imgdata[pixelPos + 2] = 0;
+            imgdata[pixelPos + 3] = 0;
           } else {
             imgdata[pixelPos + 3] = 0;
           }
--- app/scripts/layers/ForestTypeLayer.js
+++ app/scripts/layers/ForestTypeLayer.js
@@ -11,7 +11,8 @@ define([
       this._super();
       this.dataMaxZoom = 12;
       this.name = 'forestType';
-      this.urlTemplate = 'http://earthengine.google.org/static/hansen_2013/gfw_loss_year/%z/%x/%y.png';
+      //this.urlTemplate = 'http://earthengine.google.org/static/hansen_2013/gfw_loss_year/%z/%x/%y.png';
+      this.urlTemplate = 'https://s3.amazonaws.com/idn/idnft/%z/%x/%y.png';
     },
 
     filterCanvasImage: function(imgdata, w, h, z) {
